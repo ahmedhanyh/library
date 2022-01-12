a514d73546b52fd79824510e625e1a3ef2dfd7fc
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-01-12 16:23:16 +0200

    Update scripts.js
    Created displayBooks() function that adds books in myLibrary as list items dynamically to the books-list ul in index.html - Added some books manually and displayed them by invoking displayBooks function

--- js/script.js
+++ js/script.js
@@ -11,3 +11,28 @@ function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
+
+addBookToLibrary('a', 'a', 1, true);
+addBookToLibrary('a', 'a', 1, true);
+addBookToLibrary('a', 'a', 1, true);
+addBookToLibrary('a', 'a', 1, true);
+addBookToLibrary('a', 'a', 1, true);
+addBookToLibrary('a', 'a', 1, true);
+addBookToLibrary('a', 'a', 1, true);
+addBookToLibrary('a', 'a', 1, true);
+
+function displayBooks() {
+    myLibrary.forEach(book => {
+        const bookItem = document.createElement('li');
+        bookItem.innerHTML = `Title: ${book.title}<br>
+                                Author: ${book.author}<br>
+                                Number of pages: ${book.pages}<br>
+                                Status: ${book.read ? 'Read' : 'Not yet read'}<br>`;
+        bookItem.classList.add("book-item");
+        
+        document.querySelector("#books-list")
+          .appendChild(bookItem);
+    });
+}
+
+displayBooks();
