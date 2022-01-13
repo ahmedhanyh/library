85c2d24f2e34231c80e23f780efaec5c1dc96408
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-01-13 10:11:02 +0200

    Added form functionalities

--- js/script.js
+++ js/script.js
@@ -22,6 +22,9 @@ addBookToLibrary('a', 'a', 1, true);
 addBookToLibrary('a', 'a', 1, true);
 
 function displayBooks() {
+    const booksList = document.querySelector("#books-list");
+    booksList.innerHTML = '';
+    
     myLibrary.forEach(book => {
         const bookItem = document.createElement('li');
         bookItem.innerHTML = `Title: ${book.title}<br>
@@ -30,9 +33,39 @@ function displayBooks() {
                                 Status: ${book.read ? 'Read' : 'Not yet read'}<br>`;
         bookItem.classList.add("book-item");
         
-        document.querySelector("#books-list")
-          .appendChild(bookItem);
+        booksList.appendChild(bookItem);
     });
+}
+
+const newBookBtn = document.querySelector("#new-book-btn");
+const addBookBtn = document.querySelector("#add-book-btn");
+const newBookForm = document.querySelector("#new-book-form");
+
+newBookBtn.addEventListener("click", () => {
+    newBookForm.classList.add("active");
+  });
+
+addBookBtn.addEventListener("click", () => {
+    const [title, author, pages, read] = getInputtedData();
+    addBookToLibrary(title, author, pages, read);
+    newBookForm.classList.remove("active");
+    displayBooks();
+  });
+
+function getInputtedData() {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const status = document.querySelector("#status").value;
+  
+  if (title === '' || author === '' || pages === '') {
+    alert("There is one or more empty fields. All fields must be filled.");
+    return;
+  }
+
+  const read = status === "read" ? true : false;
+
+  return [title, author, pages, read];
 }
 
 displayBooks();
