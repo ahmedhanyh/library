ec64339a9fb30bb02ad3b5675d2ca3d975c70fab
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-01-15 08:28:49 -0800

    Added a hover effect to book item
    When hovering over a book item, the tooltip appears at its bottom - Linked script.js to index.html

--- js/script.js
+++ js/script.js
@@ -1,88 +1,99 @@
-const myLibrary = [];
+const bookItem = document.querySelector(".book-item");
+const tooltip = bookItem.querySelector(".tooltip");
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+bookItem.addEventListener("mouseover", () => {
+  tooltip.classList.add("active");
+});
 
-Book.prototype.toggleReadStatus = function() {
-    this.read = !this.read;
-}
+bookItem.addEventListener("mouseout", () => {
+  tooltip.classList.remove("active");
+})
 
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-}
+// const myLibrary = [];
 
-function displayBooks() {
-    const booksList = document.querySelector("#books-list");
-    booksList.innerHTML = '';
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
+
+// Book.prototype.toggleReadStatus = function() {
+//     this.read = !this.read;
+// }
+
+// function addBookToLibrary(title, author, pages, read) {
+//     const book = new Book(title, author, pages, read);
+//     myLibrary.push(book);
+// }
+
+// function displayBooks() {
+//     const booksList = document.querySelector("#books-list");
+//     booksList.innerHTML = '';
     
-    myLibrary.forEach((book, index) => {
-        const bookItem = document.createElement('li');
-        bookItem.innerHTML = `Title: ${book.title}<br>
-                              Author: ${book.author}<br>
-                              Number of pages: ${book.pages}<br>
-                              Status: ${book.read ? 'Read' : 'Not yet read'}<br>
-                              <button id="remove-book-btn">REMOVE</button>
-                              <button id="toggle-book-btn">TOGGLE READ</button>`;
+//     myLibrary.forEach((book, index) => {
+//         const bookItem = document.createElement('li');
+//         bookItem.innerHTML = `Title: ${book.title}<br>
+//                               Author: ${book.author}<br>
+//                               Number of pages: ${book.pages}<br>
+//                               Status: ${book.read ? 'Read' : 'Not yet read'}<br>
+//                               <button id="remove-book-btn">REMOVE</button>
+//                               <button id="toggle-book-btn">TOGGLE READ</button>`;
         
-        bookItem.classList.add("book-item");
-        bookItem.setAttribute("data-location", index);
+//         bookItem.classList.add("book-item");
+//         bookItem.setAttribute("data-location", index);
 
-        booksList.appendChild(bookItem);
-    });
-}
+//         booksList.appendChild(bookItem);
+//     });
+// }
 
-const booksList = document.querySelector("#books-list");
-const newBookBtn = document.querySelector("#new-book-btn");
-const addBookBtn = document.querySelector("#add-book-btn");
-const newBookForm = document.querySelector("#new-book-form");
+// const booksList = document.querySelector("#books-list");
+// const newBookBtn = document.querySelector("#new-book-btn");
+// const addBookBtn = document.querySelector("#add-book-btn");
+// const newBookForm = document.querySelector("#new-book-form");
 
-newBookBtn.addEventListener("click", () => {
-    newBookForm.classList.add("active");
-  });
+// newBookBtn.addEventListener("click", () => {
+//     newBookForm.classList.add("active");
+//   });
 
-addBookBtn.addEventListener("click", () => {
-    const [title, author, pages, read] = getInputtedData();
-    addBookToLibrary(title, author, pages, read);
-    newBookForm.classList.remove("active");
-    displayBooks();
-  });
+// addBookBtn.addEventListener("click", () => {
+//     const [title, author, pages, read] = getInputtedData();
+//     addBookToLibrary(title, author, pages, read);
+//     newBookForm.classList.remove("active");
+//     displayBooks();
+//   });
 
-function getInputtedData() {
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const status = document.querySelector("#status").value;
+// function getInputtedData() {
+//   const title = document.querySelector("#title").value;
+//   const author = document.querySelector("#author").value;
+//   const pages = document.querySelector("#pages").value;
+//   const status = document.querySelector("#status").value;
   
-  if (title === '' || author === '' || pages === '') {
-    alert("There is one or more empty fields. All fields must be filled.");
-    return;
-  }
+//   if (title === '' || author === '' || pages === '') {
+//     alert("There is one or more empty fields. All fields must be filled.");
+//     return;
+//   }
 
-  const read = status === "read" ? true : false;
+//   const read = status === "read" ? true : false;
 
-  return [title, author, pages, read];
-}
+//   return [title, author, pages, read];
+// }
 
-booksList.addEventListener("click", event => {
-    if (event.target.nodeName !== "BUTTON") {
-        return;
-    }
+// booksList.addEventListener("click", event => {
+//     if (event.target.nodeName !== "BUTTON") {
+//         return;
+//     }
 
-    const parent = event.target.parentElement;
-    const location = parent.dataset.location;
+//     const parent = event.target.parentElement;
+//     const location = parent.dataset.location;
     
-    if (event.target.textContent === "REMOVE") {
-        myLibrary.splice(location, 1);
-        parent.remove();
-    } else {
-        const book = myLibrary[location];
-        book.toggleReadStatus();
-      }
+//     if (event.target.textContent === "REMOVE") {
+//         myLibrary.splice(location, 1);
+//         parent.remove();
+//     } else {
+//         const book = myLibrary[location];
+//         book.toggleReadStatus();
+//       }
 
-    displayBooks();
-  });
+//     displayBooks();
+//   });
